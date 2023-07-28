51633d2bf2e21f84b1e2e3fbaf5460514b84ca6c
iffxxxx <iffxxxx@gmail.com>
2023-07-28 22:06:39 +0900

    Add Python File [정수 삼각형][DP]

---
+++ 1932.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/1932
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+Num = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N - 1):
+    for j in range(i + 1):
+        if j == 0:
+            Num[i + 1][j] += Num[i][0]
+        if 0 < j < i:
+            Num[i + 1][j] += max(Num[i][j - 1], Num[i][j])
+        if j == i:
+            Num[i + 1][-1] += Num[i][-1]
+
+print(max(Num[-1]))
+
+for i in Num:
+    print(i)
