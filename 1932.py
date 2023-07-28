8aa44db60605aff9b290bc9ac6553da02de62b0f
iffxxxx <iffxxxx@gmail.com>
2023-07-28 22:07:19 +0900

    Add Python File [정수 삼각형][DP]

--- 1932.py
+++ 1932.py
@@ -10,12 +10,12 @@ for i in range(N - 1):
     for j in range(i + 1):
         if j == 0:
             Num[i + 1][j] += Num[i][0]
-        if 0 < j < i:
+        if 0 < j <= i:
             Num[i + 1][j] += max(Num[i][j - 1], Num[i][j])
-        if j == i:
+        if j == 0 or j == i:
             Num[i + 1][-1] += Num[i][-1]
 
 print(max(Num[-1]))
 
-for i in Num:
-    print(i)
+# for i in Num:
+#     print(i)
